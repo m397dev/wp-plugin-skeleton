c928ac537b64cc275032cb7306e1b5034bc3b187
M397 <m397.dev@gmail.com>
2024-08-07 07:30:01 +0700

    update: Update serve, now, can generate plugin automatically

---
+++ prepare.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Prepare for plugin initialization.
+ *
+ * preg: this array contains the file name, function name and class name that
+ * will generate when initialization.
+ *
+ * preg[__plugin_name__]:           Define the plugin's file prefix.
+ * preg[__plugin_name_function__]:  Define the plugin's function prefix.
+ * preg[__Plugin_Name_Class__]:     Define the plugin's class prefix
+ *
+ * containers: This array contains all the containers that need to be scanned
+ * in the src folder.
+ *
+ * @since 1.0.0
+ */
+return [
+	'preg'       => [
+		'__plugin_name__'          => 'plugin-name',
+		'__plugin_name_function__' => 'plugin_name',
+		'__Plugin_Name_Class__'    => 'Plugin_Name',
+	],
+	'containers' => [
+		'admin',
+		'admin/functions',
+		'config',
+		'includes',
+		'includes/ajax',
+		'includes/cron',
+		'includes/databases',
+		'includes/helpers',
+		'includes/libraries',
+		'includes/repositories',
+		'includes/rest',
+		'includes/shortcodes',
+		'languages',
+		'public',
+		'public/functions',
+	],
+];
